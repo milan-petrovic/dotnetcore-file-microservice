4512bcca60ae0c52280044a111d76b7e1db0928d
Milan Petrovic <milanpdo@gmail.com>
2020-01-13 16:03:19 +0100

    Update

--- Entities/FileEntity.cs
+++ Entities/FileEntity.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,8 +9,11 @@ namespace FileMicroservice.Entities
   public class FileEntity
   {
     public string Id { get; set; }
+    [Required]
     public string Location { get; set; }
+    [Required]
     public string Context { get; set; }
+    [Required]
     public bool Available { get; set; }
   }
 }
